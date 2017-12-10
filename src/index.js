964353be338a838a5a5e2cc9af1cba564038fee6
Jack Baron <yodude107@gmail.com>
2017-12-10 14:11:14 +0000

    RPC

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 const path = require('path')
 const chokidar = require('chokidar')
 const fs = require('fs-extra')
+const DiscordRPC = require('discord-rpc')
 
 const { USERPROFILE } = process.env
 const JOURNAL_PATH = path.join(USERPROFILE, '\\Saved Games\\Frontier Developments\\Elite Dangerous')
 
-let watcher = chokidar.watch(JOURNAL_PATH)
-watcher.on('change', async () => {
-  let data = await getLatestFile(JOURNAL_PATH)
-  console.log(data)
+const CLIENT_ID = '389390500735483906'
+DiscordRPC.register(CLIENT_ID)
+const rpc = new DiscordRPC.Client({ transport: 'ipc' })
+
+rpc.on('ready', () => {
+  let watcher = chokidar.watch(JOURNAL_PATH)
+  watcher.on('change', async () => {
+    let data = await getLatestFile(JOURNAL_PATH)
+    let currentSystem = getCurrentSystem(data)
+
+    rpc.setActivity({
+      details: currentSystem.StarSystem,
+      state: 'Flying Solo',
+      largeImageKey: 'ed_logo',
+      largeImageText: 'Elite Dangerous',
+      smallImageKey: `star_${currentSystem.StarClass.toLowerCase()}`,
+      smallImageText: `Star Class: ${currentSystem.StarClass}`,
+      instance: false,
+    })
+  })
 })
+
+rpc.login(CLIENT_ID)
+  .catch(console.error)
 
 const getLatestFile = async dirPath => {
   let files = await fs.readdir(dirPath)
@@ -19,4 +39,24 @@ const getLatestFile = async dirPath => {
   return file.split('\n')
     .filter(x => x.length > 0)
     .map(JSON.parse)
+}
+
+/**
+ * @typedef {Object} SystemJump
+ * @property {string} event
+ * @property {string} JumpType
+ * @property {string} StarClass
+ * @property {string} StarSystem
+ * @property {string} timestamp
+ */
+
+/**
+ * Read last jump from Journal Files
+ * @param {any[]} data Journal Data
+ * @returns {SystemJump}
+ */
+const getCurrentSystem = data => {
+  let jumps = data.filter(x => x.event === 'StartJump' && x.JumpType === 'Hyperspace')
+  let lastJump = [...jumps].pop()
+  return lastJump
 }
