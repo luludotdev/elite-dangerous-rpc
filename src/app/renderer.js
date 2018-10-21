f7b01fb32e7fc8190c70d592a2e77d0ea30adadd
Jack Baron <jackmbaron@gmail.com>
2018-10-21 01:40:28 +0100

    Electron

---
+++ src/app/renderer.js
@@ -0,0 +1,24 @@
+/* eslint-env browser */
+
+const { ipcRenderer } = require('electron')
+
+const getLogo = horizons => horizons ?
+  'https://cdn.discordapp.com/app-assets/503220133758631954/503280110783299588.png' :
+  'https://cdn.discordapp.com/app-assets/503220133758631954/503274245820776448.png'
+
+const logoDOM = document.getElementById('logo')
+const systemDOM = document.getElementById('system')
+const stateDOM = document.getElementById('state')
+
+ipcRenderer.on('details', (_, details) => {
+  const logo = getLogo(details.largeImageKey === 'ed_logo_h')
+  const system = details.details
+
+  const state = details.partyId === undefined ?
+    details.state :
+    `${details.state} (${details.partySize} of ${details.partyMax})`
+
+  logoDOM.src = logo
+  systemDOM.innerHTML = system
+  stateDOM.innerHTML = state
+})
---
+++ src/watcher.js
@@ -0,0 +1,56 @@
+const uuid = require('uuid/v4')
+const chokidar = require('chokidar')
+const { EventEmitter } = require('events')
+const { JOURNAL_PATH } = require('./constants.js')
+const { getLatestFile } = require('./file.js')
+const { getCurrentSystem, getWingStatus, getDockedStatus, getHorizons, getTouchdownStatus } = require('./parse.js')
+
+class Watcher extends EventEmitter {
+  constructor () {
+    super()
+
+    this._watcher = chokidar.watch(JOURNAL_PATH, { usePolling: true })
+
+    this._watcher.on('change', async () => {
+      const data = await getLatestFile(JOURNAL_PATH)
+
+      try {
+        const currentSystem = getCurrentSystem(data)
+        const wingData = getWingStatus(data)
+        const docked = getDockedStatus(data)
+        const horizons = getHorizons(data)
+        const touchdown = getTouchdownStatus(data)
+
+        let details = {
+          details: docked ? `Docked at ${docked.StationName} (${docked.StationType})` : currentSystem.StarSystem,
+          state: touchdown === 'srv' ?
+            'Piloting an SRV' :
+            touchdown === 'touchdown' ?
+              'Touched Down' :
+              'Flying Solo',
+
+          largeImageKey: horizons ? 'ed_logo_h' : 'ed_logo',
+          largeImageText: horizons ? 'Elite Dangerous: Horizons' : 'Elite Dangerous',
+
+          // Broken, might revisit
+          // smallImageKey: `star_${currentSystem.StarClass.toLowerCase()}`,
+          // smallImageText: `Star Class: ${currentSystem.StarClass}`,
+        }
+
+        // Override state in wing
+        if (wingData) {
+          details.state = 'In Wing'
+          details.partySize = wingData
+          details.partyMax = 4
+          details.partyId = uuid()
+        }
+
+        this.emit('details', details)
+      } catch (err) {
+        console.error(err)
+      }
+    })
+  }
+}
+
+module.exports = { Watcher }
